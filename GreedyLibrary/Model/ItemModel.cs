2ff281f72700b625304b5bbd9dba414c747b5d58
Matthew-Foley-Student <foleymat000@gmail.com>
2026-01-04 00:18:59 -0800

    Add files via upload

---
+++ GreedyLibrary/Model/ItemModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyLibrary.Model
+{
+    public class ItemModel
+    {
+        public ItemModel(string name, int price, int weight, int amount)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+            Amount = amount;
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }   
+        public int Amount { get; set; }
+        
+        public ItemModel() { }
+    }
+
+
+    }
